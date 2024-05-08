f6581cfc2dc2c3f3e9c405fe298edc187851082d
Ashutosh Jha <120783224+ashutoshjha31@users.noreply.github.com>
2024-05-08 17:46:29 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="ashutoshjha34873@gmail.com"
+var accountPassword="12345"
+//accountCity="Jaipur"
+accountEmail="gdkjsad@gmail.com"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountEmail);
